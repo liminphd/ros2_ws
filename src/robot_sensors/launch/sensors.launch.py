e418e8c78a663f35cb538ced2f31848c825617aa
Min Li <liminphd@gmail.com>
2026-07-27 11:40:07 +0300

    Refactor platform sensor bringup architecture

---
+++ src/robot_sensors/launch/sensors.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+
+def generate_launch_description():
+
+    package_share = get_package_share_directory("robot_sensors")
+
+    velodyne_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                package_share,
+                "launch",
+                "velodyne.launch.py",
+            )
+        )
+    )
+
+    return LaunchDescription([
+        velodyne_launch,
+    ])
