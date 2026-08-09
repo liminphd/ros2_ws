5ae1bbf96f98e9210e2963dc29f701104c865289
Min Li <liminphd@gmail.com>
2026-08-09 20:02:12 +0300

    Update skid-steer robot model and SBG configuration

--- src/my_robot_bringup/launch/description.launch.py
+++ src/my_robot_bringup/launch/description.launch.py
@@ -1,41 +1,54 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.substitutions import Command, FindExecutable
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
-    robot_description_path = os.path.join(
-        get_package_share_directory('my_robot_description'),
-        'urdf',
-        'robot.urdf.xacro'
+
+    xacro_file = PathJoinSubstitution(
+        [
+            FindPackageShare("my_robot_description"),
+            "urdf",
+            "robot.urdf.xacro",
+        ]
     )
 
-    robot_description = {
-        'robot_description': Command([
-            'xacro ',
-            robot_description_path
-        ])
-    }
-
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        output='screen'
+    robot_description = ParameterValue(
+        Command(
+            [
+                FindExecutable(name="xacro"),
+                " ",
+                xacro_file,
+            ]
+        ),
+        value_type=str,
     )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": robot_description,
+            }
+        ],
     )
 
-    return LaunchDescription([
-        joint_state_publisher_node,
-        robot_state_publisher_node
-    ])
+    joint_state_publisher = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [
+            joint_state_publisher,
+            robot_state_publisher,
+        ]
+    )
